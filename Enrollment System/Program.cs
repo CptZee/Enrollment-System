a8c5955bee1afca216c36e1e869f3044ddaef1e9
CptZ <ajmission28@gmail.com>
2023-01-14 17:22:51 +0800

    Added the execution of Subjects and Schedules creation and loading.

--- Enrollment System/Program.cs
+++ Enrollment System/Program.cs
@@ -18,38 +18,46 @@ namespace Enrollment_System
             Thread AdminDBThread = new Thread(new ThreadStart(DatabaseHelper.createAdminTable));
             Thread ApplicationFormDBThread = new Thread(new ThreadStart(DatabaseHelper.createApplicationFormTable));
             Thread ContactDBThread = new Thread(new ThreadStart(DatabaseHelper.createContactTable));
-            Thread CoursesDBThread = new Thread(new ThreadStart(DatabaseHelper.createCoursesTable));
             Thread GuardianDBThread = new Thread(new ThreadStart(DatabaseHelper.createGuardianTable));
             Thread SchoolHistoryDBThread = new Thread(new ThreadStart(DatabaseHelper.createSchoolHistoryTable));
             Thread StudentDBThread = new Thread(new ThreadStart(DatabaseHelper.createStudentsTable));
+            Thread SubjectDBThread = new Thread(new ThreadStart(DatabaseHelper.createStudentsTable));
+            Thread ScheduleDBThread = new Thread(new ThreadStart(DatabaseHelper.createSchedulesTable));
+            Thread CoursesDBThread = new Thread(new ThreadStart(DatabaseHelper.createCoursesTable));
 
 
             Thread AddressLoadThread = new Thread(new ThreadStart(DatabaseHelper.loadAddresses));
             Thread AdminLoadThread = new Thread(new ThreadStart(DatabaseHelper.loadAdmins));
             Thread ApplicationFormLoadThread = new Thread(new ThreadStart(DatabaseHelper.loadApplicationForms));
             Thread ContactLoadThread = new Thread(new ThreadStart(DatabaseHelper.loadContacts));
-            Thread CoursesLoadThread = new Thread(new ThreadStart(DatabaseHelper.loadCourses));
             Thread GuardianLoadThread = new Thread(new ThreadStart(DatabaseHelper.loadGuardians));
             Thread SchoolHistoryLoadThread = new Thread(new ThreadStart(DatabaseHelper.loadSchoolHistory));
             Thread StudentLoadThread = new Thread(new ThreadStart(DatabaseHelper.loadStudents));
+            Thread SubjectLoadThread = new Thread(new ThreadStart(DatabaseHelper.loadSubjects));
+            Thread ScheduleLoadThread = new Thread(new ThreadStart(DatabaseHelper.loadSchedules));
+            Thread CoursesLoadThread = new Thread(new ThreadStart(DatabaseHelper.loadCourses));
 
             AddressDBThread.Start();
             AdminDBThread.Start();
             ApplicationFormDBThread.Start();
             ContactDBThread.Start();
-            CoursesDBThread.Start();
             GuardianDBThread.Start();
             SchoolHistoryDBThread.Start();
             StudentDBThread.Start();
+            SubjectDBThread.Start();
+            ScheduleDBThread.Start();
+            CoursesDBThread.Start();
 
             AddressLoadThread.Start();
             AdminLoadThread.Start();
             ApplicationFormLoadThread.Start();
             ContactLoadThread.Start();
-            CoursesLoadThread.Start();
             GuardianLoadThread.Start();
             SchoolHistoryLoadThread.Start();
             StudentLoadThread.Start();
+            SubjectLoadThread.Start();
+            ScheduleLoadThread.Start();
+            CoursesLoadThread.Start();
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
